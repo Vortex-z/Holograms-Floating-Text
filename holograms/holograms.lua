72bb83de37b7282105769242bead59db237c111c
Meh <41191177+Vortex-z@users.noreply.github.com>
2018-10-29 05:16:56 +0100

    Update holograms.lua

--- holograms/holograms.lua
+++ holograms/holograms.lua
@@ -3,6 +3,7 @@
 -- Just put in the coordinates you get when standing on the ground, it's above the ground then
 -- By default, you only see the hologram when you are within 10m of it.
 -- The Default holograms are at the Observatory.
+-- You can change the font by changing the first number after the "text".
 
 Citizen.CreateThread(function()
     Holograms()
@@ -11,12 +12,14 @@ end)
 function Holograms()
 		while true do
 			Citizen.Wait(0)
+
 			-- Hologram No. 1
 		if GetDistanceBetweenCoords( -408.50, 1164.00, 326.00, GetEntityCoords(LocalPed())) < 10.0 then
 			Draw3DText( -408.50, 1164.00, 326.00  -1.400, "Your",4,0.15,0.1)
 			Draw3DText( -408.50, 1164.00, 326.00  -1.600, "text",4,0.15,0.1)
 			Draw3DText( -408.50, 1164.00, 326.00  -1.800, "here",4,0.15,0.1)		
 		end
+
 			--Hologram No. 2
 		if GetDistanceBetweenCoords( -419.50, 1166.00, 326.00, GetEntityCoords(LocalPed())) < 10.0 then
 			Draw3DText( -419.50, 1166.00, 326.00  -1.400, "Discord: discord.me/HailTheSnail",4,0.15,0.1)
