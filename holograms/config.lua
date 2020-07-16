2a3261438bd466bfecac52c92aa16aed2971f505
JulienSanterre <52453095+JulienSanterre@users.noreply.github.com>
2020-07-16 03:40:46 +0200

    Create config.lua

---
+++ holograms/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.Show = 10.0 -- Distance min to show
+
+-- some exemples
+Config.List = {
+	{ x = -267.4, y = -959.64, z = 31.24, data = "Oh le beau texte", FrontId = 4, r = 255, g = 255, b = 255, alpha = 255, ScaleX = 0.1, ScaleY = 0.1 },
+	{ x = -419.50, y = 1166.00, z = 326.00, data = "Oh le beau texte", FrontId = 4, r = 255, g = 255, b = 255, alpha = 255, ScaleX = 0.1, ScaleY = 0.1 },
+}
